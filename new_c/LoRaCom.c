3c6d54e7dd4b6a63b453f544389a581c7d586f11
Bubbadime <146256626+Bubbadime@users.noreply.github.com>
2025-05-02 12:11:10 -0500

    Magic and opts

--- new_c/LoRaCom.c
+++ new_c/LoRaCom.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <unistd.h>;
 #include <stdio.h>
 #include "LoRa.h"
 #include "PlatformLoRa.h"
@@ -6,6 +7,10 @@
 #pragma pack(push)
 #pragma pack(1)
 typedef struct PayloadHeader PayloadHeader;
+
+typedef enum HeaderMagic1 HeaderMagic1;
+typedef enum HeaderMagic2 HeaderMagic2;
+
 struct PayloadHeader {
     union {
         uint16_t magic16;
@@ -15,6 +20,19 @@ struct PayloadHeader {
     uint32_t callsign;
     uint8_t imageId;
     uint16_t packetId;
+};
+
+enum HeaderMagic1 {
+    Sync1 = 0x88,
+    Ssdv = 0x55,
+    Ack1 = '!'
+};
+enum HeaderMagic2 {
+    Sync2 = 0x00,
+    Ssdv_Invalid = 0x65,
+    Ssdv_Normal = 0x66,
+    Ssdv_No_Fec = 0x67,
+    Ack2 = '!'
 };
 
 #pragma pack(pop)
@@ -27,18 +45,34 @@ PayloadHeader dec_header(uint8_t* packet) {
     id[1] = tmp;
     return result;
 }
-void print_header(PayloadHeader header) {
-    printf("Magic: 0x%hx, sign: 0x%x, image: %hhu, packet: %hu\n", header.magic16, header.callsign, header.imageId, header.packetId);
+void enc_header(PayloadHeader* h) {
+    uint8_t *id = (void*)&(h->packetId);
+    uint8_t tmp;
+    tmp = id[0];
+    id[0] = id[1];
+    id[1] = tmp;
+    return;
 }
-uint32_t main() {
+void print_header(PayloadHeader h) {
+    printf("Magic: 0x%hx, sign: 0x%x, image: %hhu, packet: %hu\n", h.magic16, h.callsign, h.imageId, h.packetId);
+}
+
+int main(int argc, char** argv) {
+    // Parse opts
+    char c = 0;
+    while ((c = getopt(argc, argv, "sr") != -1)) {
+        printf("opt %c", c);
+
+    }
+    printf("c\n");
     FILE* fd = fopen("out2.ssdv", "rb");
     uint8_t buf[2048];
-	printf("Size of header: %p\n", sizeof(PayloadHeader));
-for (uint32_t i = 0; i< 8; i++) {
+	printf("Size of header: %u\n", sizeof(PayloadHeader));
+    for (uint32_t i = 0; i< 8; i++) {
 
     fread(buf, 1, 256, fd);
     PayloadHeader h = dec_header(buf);
     print_header(h);
-}
+    }
     return 0;
 }
